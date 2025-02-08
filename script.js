0e7635cf343922a194f4510ca0179d49308ea36a
KeerthiKappera <159817502+KeerthiKappera@users.noreply.github.com>
2025-02-08 18:00:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.nav-menu a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
